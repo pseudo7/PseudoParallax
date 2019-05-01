6b80b1c6792eb649575af4a914ef38d409f03cfb
Pseudo <enikhil7@gmail.com>
2019-05-01 18:39:39 +0530

    Init Commit

---
+++ Assets/Scripts/GyroTranslator.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class GyroTranslator : MonoBehaviour
+{
+    [SerializeField] float editorDampening = 10;
+    [SerializeField] float deviceDampening = 5;
+
+    static float zPos, dampening;
+
+    Vector3 lastPos, delta;
+    float yRotation, xRotation;
+    bool isEditor;
+
+    void Start()
+    {
+        zPos = transform.position.z;
+        Input.gyro.enabled = true;
+        isEditor = Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.OSXEditor;
+        dampening = 1 / (isEditor ? editorDampening : deviceDampening);
+    }
+
+    void Update()
+    {
+        if (isEditor)
+        {
+            if (Input.GetMouseButtonDown(0))
+                lastPos = Input.mousePosition;
+            else if (Input.GetMouseButton(0))
+            {
+                delta = Input.mousePosition - lastPos;
+
+                transform.Translate(delta * dampening);
+
+                lastPos = Input.mousePosition;
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0)) ResetParallax();
+
+            xRotation += -Input.gyro.rotationRateUnbiased.x;
+            yRotation += -Input.gyro.rotationRateUnbiased.y;
+            transform.position = new Vector3(yRotation * dampening, xRotation * dampening, zPos);
+        }
+    }
+
+    void ResetParallax()
+    {
+        xRotation = yRotation = 0;
+    }
+}
---
+++ Assets/Scripts/PseudoParallax.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PseudoParallax : MonoBehaviour
+{
+    static Camera mainCam;
+
+    Vector3 origCamPos;
+
+    #region Constants
+    const float FOV = 60;
+    const float NEAR_CLIP_PLANE = 0.05F;
+    const float FAR_CLIP_PLANE = 20.0F;
+
+    const float PARALLAX_STEP = 0.012F;
+    const float HORIZONTAL_OBLIQUE = 0.06F;
+    const float VERTICAL_OBLIQUE = 0.03F;
+    #endregion
+
+    void Start()
+    {
+        SetupCamera();
+    }
+
+    void LateUpdate()
+    {
+        mainCam.projectionMatrix = PerspectiveOffCenter(-HORIZONTAL_OBLIQUE + mainCam.transform.position.x * -PARALLAX_STEP,
+            HORIZONTAL_OBLIQUE + mainCam.transform.position.x * -PARALLAX_STEP,
+            -VERTICAL_OBLIQUE + mainCam.transform.position.y * -PARALLAX_STEP,
+            VERTICAL_OBLIQUE + mainCam.transform.position.y * -PARALLAX_STEP,
+            NEAR_CLIP_PLANE, FAR_CLIP_PLANE);
+    }
+
+    void SetupCamera()
+    {
+        mainCam = Camera.main;
+        mainCam.orthographic = false;
+        mainCam.fieldOfView = FOV;
+        mainCam.nearClipPlane = NEAR_CLIP_PLANE;
+        mainCam.farClipPlane = FAR_CLIP_PLANE;
+    }
+
+    static Matrix4x4 PerspectiveOffCenter(float left, float right, float bottom, float top, float near, float far)
+    {
+        float x = 2.0F * near / (right - left);
+        float y = 2.0F * near / (top - bottom);
+        float a = (right + left) / (right - left);
+        float b = (top + bottom) / (top - bottom);
+        float c = -(far + near) / (far - near);
+        float d = -(2.0F * far * near) / (far - near);
+        float e = -1.0F;
+
+        Matrix4x4 m = new Matrix4x4()
+        {
+            m00 = x,
+            m01 = 0,
+            m02 = a,
+            m03 = 0,
+
+            m10 = 0,
+            m11 = y,
+            m12 = b,
+            m13 = 0,
+
+            m20 = 0,
+            m21 = 0,
+            m22 = c,
+            m23 = d,
+
+            m30 = 0,
+            m31 = 0,
+            m32 = e,
+            m33 = 0,
+        };
+        return m;
+    }
+}
